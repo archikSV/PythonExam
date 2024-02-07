c06508efccdcff1c2c85207687049582e3823053
SalchukArtem <archik.sv@gmail.com>
2024-02-07 20:28:40 +0200

    Part 1: task 3 completed

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
 print("Part 1: Python Basic")
-print("Task 1")
+print("Task 3")
 
 """
-task
+Напишіть програму, яка приймає рядок від користувача і
+виводить кожну літеру рядка на окремому рядку.
 """
+
+string = input("Print the string: ")
+for char in string:
+    print(char)

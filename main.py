0f0f58cb8da7cd51b19cd3ba8bd3b85cfdc883cc
SalchukArtem <archik.sv@gmail.com>
2024-02-07 20:27:34 +0200

    Part 1: task 2 completed

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
 print("Part 1: Python Basic")
-print("Task 1")
+print("Task 2")
 
 """
-task
+Напишіть програму, для знаходження суми всіх парних
+чисел від 1 до 100.
 """
+
+# Initialize sum
+total_sum = 0
+
+# Iterate through numbers from 1 to 100
+for i in range(1, 101):
+    # Check if the number is even
+    if i % 2 == 0:
+        # Add the even number to the total sum
+        total_sum += i
+
+# Print the total sum of even numbers
+print(total_sum)

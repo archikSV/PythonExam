8f995861294e210fa0b9e91b84ed6f037dc2591a
SalchukArtem <archik.sv@gmail.com>
2024-02-07 20:29:56 +0200

    Part 1: task 4 completed

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 print("Part 1: Python Basic")
-print("Task 1")
+print("Task 4")
 
 """
-task
+Напишіть програму, яка створює список цілих чисел та
+виводить новий список, який містить лише парні числа з
+вихідного списку.
 """
+
+original_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+# Printing a new list containing only the even numbers from the original list
+even_numbers_list = [num for num in original_list if num % 2 == 0]
+print(even_numbers_list)

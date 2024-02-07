3f3a62af813dde14d79fbbc3f6936173b98dc8da
SalchukArtem <archik.sv@gmail.com>
2024-02-07 20:32:06 +0200

    Part 1: task 5 completed

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 print("Part 1: Python Basic")
-print("Task 1")
+print("Task 5")
 
 """
-task
+Напишіть функцію, яка приймає список рядків від
+користувача і повертає новий список, що містить лише
+рядки, що починаються з великої літери.
 """
+
+def filter_capitalized_strings(input_list):
+    return [s for s in input_list if s[0].isupper()]
+
+input_list = ["Jeremy", "Hobkins", "is", "the", "Best"]
+result = filter_capitalized_strings(input_list)
+print(result)  # Output: ['Jeremy', 'Hobkins', 'Best']

749a31c03cd07ee5fd01ccefbdf031d829f1aacf
SalchukArtem <archik.sv@gmail.com>
2024-02-07 20:34:09 +0200

    Part 1: task 6 completed

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 print("Part 1: Python Basic")
-print("Task 1")
+print("Task 6")
 
 """
-task
+Напишіть функцію, яка приймає список рядків від
+користувача і повертає новий список, що містить лише
+рядки, які містять слово "Python".
 """
+
+def filter_python_strings(input_list):
+    return [string for string in input_list if "Python" in string]
+
+input_list = ["I love Python", "Python is great", "Java is not Python", "Java is not Great"]
+filtered_list = filter_python_strings(input_list)
+print(filtered_list) # Output: ['I love Python', 'Python is great', 'Java is not Python']
